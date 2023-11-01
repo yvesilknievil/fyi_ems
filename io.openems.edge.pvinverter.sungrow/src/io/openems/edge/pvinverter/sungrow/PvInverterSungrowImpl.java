ea3054e062d722a6a36fbe619dfe8bd80bc5c0a7
Thomas Sicking <91258335+tsicking@users.noreply.github.com>
2023-11-01 10:26:02 +0100

    Formatting, small refactoring.

--- io.openems.edge.pvinverter.sungrow/src/io/openems/edge/pvinverter/sungrow/PvInverterSungrowImpl.java
+++ io.openems.edge.pvinverter.sungrow/src/io/openems/edge/pvinverter/sungrow/PvInverterSungrowImpl.java
@@ -79,12 +79,7 @@ public class PvInverterSungrowImpl extends AbstractOpenemsModbusComponent
 				"Modbus", config.modbus_id())) {
 			return;
 		}
-		this.getActivePowerChannel().onSetNextValue(value -> { //
-			var powerPerPhase = value.orElse(0) / 3;
-			this._setActivePowerL1(powerPerPhase);
-			this._setActivePowerL2(powerPerPhase);
-			this._setActivePowerL3(powerPerPhase);
-		});
+		ElectricityMeter.calculatePhasesFromActivePower(this);
 	}
 
 	@Deactivate
@@ -95,57 +90,59 @@ public class PvInverterSungrowImpl extends AbstractOpenemsModbusComponent
 	@Override
 	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
 
-		return new ModbusProtocol(this, new FC4ReadInputRegistersTask(4989, Priority.HIGH, //
-				m(PvInverterSungrow.ChannelId.SERIAL_NUMBER, new StringWordElement(4989, 10)), //
-				new DummyRegisterElement(4999), // Device Type Code
-				m(PvInverterSungrow.ChannelId.NOMINAL_OUTPUT_POWER, new UnsignedWordElement(5000), //
-						SCALE_FACTOR_2), //
-				new DummyRegisterElement(5001), // Outout type
-				m(PvInverterSungrow.ChannelId.DAILY_ENERGY, new UnsignedWordElement(5002), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY,
-						new UnsignedDoublewordElement(5003).wordOrder(WordOrder.LSWMSW), //
-						SCALE_FACTOR_3), //
-				m(PvInverterSungrow.ChannelId.TOTAL_RUNNING_TIME,
-						new UnsignedDoublewordElement(5005).wordOrder(WordOrder.LSWMSW)), //
-				m(PvInverterSungrow.ChannelId.INTERNAL_TEMPERATURE, new SignedWordElement(5007)), //
-				m(PvInverterSungrow.ChannelId.APPARENT_POWER,
-						new UnsignedDoublewordElement(5008).wordOrder(WordOrder.LSWMSW)), //
-				m(PvInverterSungrow.ChannelId.DC_VOLTAGE_1, new UnsignedWordElement(5010), //
-						SCALE_FACTOR_MINUS_1), //
-				m(PvInverterSungrow.ChannelId.DC_CURRENT_1, new UnsignedWordElement(5011), //
-						SCALE_FACTOR_2), //
-				m(PvInverterSungrow.ChannelId.DC_VOLTAGE_2, new UnsignedWordElement(5012), //
-						SCALE_FACTOR_MINUS_1), //
-				m(PvInverterSungrow.ChannelId.DC_CURRENT_2, new UnsignedWordElement(5013), //
-						SCALE_FACTOR_2), //
-				m(PvInverterSungrow.ChannelId.DC_VOLTAGE_3, new UnsignedWordElement(5014), //
-						SCALE_FACTOR_MINUS_1), //
-				m(PvInverterSungrow.ChannelId.DC_CURRENT_3, new UnsignedWordElement(5015), //
-						SCALE_FACTOR_2), //
-				m(PvInverterSungrow.ChannelId.DC_POWER,
-						new UnsignedDoublewordElement(5016).wordOrder(WordOrder.LSWMSW)), //
-				m(ElectricityMeter.ChannelId.VOLTAGE_L1, new UnsignedWordElement(5018), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.VOLTAGE_L2, new UnsignedWordElement(5019), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.VOLTAGE_L3, new UnsignedWordElement(5020), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(5021), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(5022), //
-						SCALE_FACTOR_2), //
-				m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(5023), //
-						SCALE_FACTOR_2), //
-				new DummyRegisterElement(5024, 5029), // Reserved
-				m(ElectricityMeter.ChannelId.ACTIVE_POWER,
-						new UnsignedDoublewordElement(5030).wordOrder(WordOrder.LSWMSW)), //
-				m(ElectricityMeter.ChannelId.REACTIVE_POWER,
-						new SignedDoublewordElement(5032).wordOrder(WordOrder.LSWMSW)), //
-				m(PvInverterSungrow.ChannelId.POWER_FACTOR, new SignedWordElement(5034)), //
-				new DummyRegisterElement(5035, 5036), // 5035: Frequency, read from another register, 5036: reserved
-				m(PvInverterSungrow.ChannelId.WORK_STATE, new UnsignedWordElement(5037)) //
-		),
+		return new ModbusProtocol(this, //
+				new FC4ReadInputRegistersTask(4989, Priority.HIGH, //
+						m(PvInverterSungrow.ChannelId.SERIAL_NUMBER, new StringWordElement(4989, 10)), //
+						new DummyRegisterElement(4999), // Device Type Code
+						m(PvInverterSungrow.ChannelId.NOMINAL_OUTPUT_POWER, new UnsignedWordElement(5000), //
+								SCALE_FACTOR_2), //
+						new DummyRegisterElement(5001), // Outout type
+						m(PvInverterSungrow.ChannelId.DAILY_ENERGY, new UnsignedWordElement(5002), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY,
+								new UnsignedDoublewordElement(5003).wordOrder(WordOrder.LSWMSW), //
+								SCALE_FACTOR_3), //
+						m(PvInverterSungrow.ChannelId.TOTAL_RUNNING_TIME,
+								new UnsignedDoublewordElement(5005).wordOrder(WordOrder.LSWMSW)), //
+						m(PvInverterSungrow.ChannelId.INTERNAL_TEMPERATURE, new SignedWordElement(5007)), //
+						m(PvInverterSungrow.ChannelId.APPARENT_POWER,
+								new UnsignedDoublewordElement(5008).wordOrder(WordOrder.LSWMSW)), //
+						m(PvInverterSungrow.ChannelId.DC_VOLTAGE_1, new UnsignedWordElement(5010), //
+								SCALE_FACTOR_MINUS_1), //
+						m(PvInverterSungrow.ChannelId.DC_CURRENT_1, new UnsignedWordElement(5011), //
+								SCALE_FACTOR_2), //
+						m(PvInverterSungrow.ChannelId.DC_VOLTAGE_2, new UnsignedWordElement(5012), //
+								SCALE_FACTOR_MINUS_1), //
+						m(PvInverterSungrow.ChannelId.DC_CURRENT_2, new UnsignedWordElement(5013), //
+								SCALE_FACTOR_2), //
+						m(PvInverterSungrow.ChannelId.DC_VOLTAGE_3, new UnsignedWordElement(5014), //
+								SCALE_FACTOR_MINUS_1), //
+						m(PvInverterSungrow.ChannelId.DC_CURRENT_3, new UnsignedWordElement(5015), //
+								SCALE_FACTOR_2), //
+						m(PvInverterSungrow.ChannelId.DC_POWER,
+								new UnsignedDoublewordElement(5016).wordOrder(WordOrder.LSWMSW)), //
+						m(ElectricityMeter.ChannelId.VOLTAGE_L1, new UnsignedWordElement(5018), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.VOLTAGE_L2, new UnsignedWordElement(5019), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.VOLTAGE_L3, new UnsignedWordElement(5020), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(5021), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(5022), //
+								SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(5023), //
+								SCALE_FACTOR_2), //
+						new DummyRegisterElement(5024, 5029), // Reserved
+						m(ElectricityMeter.ChannelId.ACTIVE_POWER,
+								new UnsignedDoublewordElement(5030).wordOrder(WordOrder.LSWMSW)), //
+						m(ElectricityMeter.ChannelId.REACTIVE_POWER,
+								new SignedDoublewordElement(5032).wordOrder(WordOrder.LSWMSW)), //
+						m(PvInverterSungrow.ChannelId.POWER_FACTOR, new SignedWordElement(5034)), //
+						new DummyRegisterElement(5035, 5036), // 5035: Frequency, read from another register, 5036:
+																// reserved
+						m(PvInverterSungrow.ChannelId.WORK_STATE, new UnsignedWordElement(5037)) //
+				),
 
 				new FC4ReadInputRegistersTask(5145, Priority.LOW, //
 						m(PvInverterSungrow.ChannelId.NEGATIVE_VOLTAGE_TO_THE_GROUND, new SignedWordElement(5145), //
