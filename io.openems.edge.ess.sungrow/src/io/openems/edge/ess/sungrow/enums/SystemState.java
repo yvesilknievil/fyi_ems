1389510797e32ca7d1f1bfb170f08820b76bbc7f
Tim Stöcker <tim.stoecker@opernikus.de>
2023-11-02 14:42:15 +0100

    Removed duplicate Enum Value

--- io.openems.edge.ess.sungrow/src/io/openems/edge/ess/sungrow/enums/SystemState.java
+++ io.openems.edge.ess.sungrow/src/io/openems/edge/ess/sungrow/enums/SystemState.java
@@ -12,7 +12,7 @@ public enum SystemState implements OptionsEnum {
     FAULT(0x0100, "Fault"), //
     RUNNING_IN_MAINTAIN_MODE(0x0400, "Running in maintain mode"), //
     RUNNING_IN_FORCED_MODE(0x0800, "Running in forced mode"), //
-    RUNNING_IN_OFF_GRID_MODE(0x0400, "Running in off-grid mode"), //
+    //RUNNING_IN_OFF_GRID_MODE(0x0400, "Running in off-grid mode"), //
     RESTARTING(0x2501, "Restarting"), //
     RUNNING_IN_EXTERNAL_EMS_MODE(0x4000, "Running in External EMS mode") //
     ;
